cb318f769ca03fdcabd13c1e3c2d4599a9fc2316
Eryk-Kanownik <eryk.patryk.kanownik@gmail.com>
2024-04-12 19:36:08 +0200

    f

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const path = require("path");
 dotenv.config();
 
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
