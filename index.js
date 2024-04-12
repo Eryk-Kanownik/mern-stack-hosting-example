75d0c099b61398901df4a613546beedd58d529e4
Eryk-Kanownik <eryk.patryk.kanownik@gmail.com>
2024-04-12 17:47:45 +0200

    Hello

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const app = express();
 
-if (process.env.NODE_ENV) {
+if (process.env.NODE_ENV === "Production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
