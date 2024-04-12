c1661f14aeb0b21923ec133fc4e7afdeaa610dc0
Eryk-Kanownik <eryk.patryk.kanownik@gmail.com>
2024-04-12 19:56:51 +0200

    Hello

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const dotenv = require("dotenv");
 const path = require("path");
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Not in production mode");
   });
 }
 
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`http://localhost:${PORT}`)
+);
